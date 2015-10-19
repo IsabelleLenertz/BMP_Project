bad5243a9d503a9950e0e3e8db0c26d90303acdb
Isabelle Delmas <isabelle.lenertz@gmail.com>
2015-10-19 10:37:42 -0700

    Calculation of the padding bytes per lines works for 24bites BMP

--- src/BMP_Project.cpp
+++ src/BMP_Project.cpp
@@ -14,8 +14,12 @@ using namespace std;
 int main() {
 	ifstream reader;
 	int* pWidth = new int;
+	int* pHeight = new int;
+	int* pBitesPerPixel = new int;
+	int bytePerPixel = 0;
+	int paddingPerLine = 0;
 
-	reader.open("FF7.bmp", ios::binary);
+	reader.open("FF49-20.bmp", ios::binary);
 	if( reader.is_open() == true )
 	{
 		cout << "The file was found." << endl;
@@ -31,6 +35,24 @@ int main() {
 	reader.seekg(18);
 	reader.read(reinterpret_cast<char*>(pWidth), 4);
 	cout << "The width of the image is: " << *pWidth << endl;
+
+	// Reads the height of the image
+	reader.seekg(22);
+	reader.read(reinterpret_cast<char*>(pHeight), 4);
+	cout << "The height of the image is: "<< *pHeight << endl;
+
+	// Reads the number of bites per pixel
+	reader.seekg(28);
+	reader.read(reinterpret_cast<char*>(pBitesPerPixel), 4);
+	cout << "Bites per pixel: " << *pBitesPerPixel<<  endl;
+
+	// Calculate the number of Bytes per pixel
+	bytePerPixel = *pBitesPerPixel/8;
+	cout <<"Bytes per pixel: " << bytePerPixel << endl;
+
+	// Calculate the number f padding bytes per line (a line has to end with a multiple of 4 bytes)
+	paddingPerLine = 4-(*pWidth)*bytePerPixel%4;
+	cout << "Padding per line: " << paddingPerLine << endl;
 
 
 	return 0;
