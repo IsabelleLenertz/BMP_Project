af16442035cf7e41827fd8e5ad5d46e48217b0db
Isabelle Delmas <isabelle.lenertz@gmail.com>
2015-10-19 13:58:39 -0700

    reading is ok, but writing does not work

--- src/BMP_Project.cpp
+++ src/BMP_Project.cpp
@@ -13,23 +13,28 @@ using namespace std;
 
 int main() {
 	ifstream reader;
+	ofstream writer;
 	int* pWidth = new int;
 	int* pHeight = new int;
 	int* pBitesPerPixel = new int;
 	int bytePerPixel = 0;
 	int paddingPerLine = 0;
+	int paddingAvailable =0;
+	int pixelArrayStartingAddress = 54; // for 24 bites BMP only
+	char* pMessage = new char;
+	*pMessage = 'a';
 
 	reader.open("FF49-20.bmp", ios::binary);
 	if( reader.is_open() == true )
 	{
 		cout << "The file was found." << endl;
 	}
-	else
+	/*else
 	{
 		cout << "The file could not be open." << endl;
 		cout << "Emergency exit." << endl;
 		return 0;
-	}
+	}*/
 
 	// Reads the width of the image
 	reader.seekg(18);
@@ -50,10 +55,29 @@ int main() {
 	bytePerPixel = *pBitesPerPixel/8;
 	cout <<"Bytes per pixel: " << bytePerPixel << endl;
 
-	// Calculate the number f padding bytes per line (a line has to end with a multiple of 4 bytes)
+	// Calculate the number of padding bytes per line (a line has to end with a multiple of 4 bytes)
 	paddingPerLine = 4-(*pWidth)*bytePerPixel%4;
 	cout << "Padding per line: " << paddingPerLine << endl;
 
+	// Calculate the number of padding bytes in the entire file.
+	paddingAvailable = paddingPerLine*(*pHeight);
+	cout << "padding in the image: " << paddingAvailable << endl;
 
+	// Closes the reader
+	reader.close();
+
+	// Inserting a char into the padding bytes.
+	writer.open("FF49-20.bmp", ios::binary);
+	if (writer.is_open() == true){
+		cout << "File was open, input : a" << endl;
+		writer.seekp(pixelArrayStartingAddress);
+		writer.write(pMessage, 1);
+	}
+	else{
+		cout << "Could not open file for input" << endl;
+	}
+
+	//Closes the writer
+	writer.close();
 	return 0;
 }
